bd7d37b235509d8543d310c0f7c0c031990da5d2
darkinfusion <daddyfire366@gmail.com>
2018-11-15 13:33:59 -0700

    add

---
+++ Android.mk
@@ -0,0 +1,82 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+        $(call all-logtags-files-under, src)
+
+LOCAL_MODULE := settings-logtags
+
+include $(BUILD_STATIC_JAVA_LIBRARY)
+
+# Build the Settings APK
+include $(CLEAR_VARS)
+
+LOCAL_PACKAGE_NAME := Settings
+LOCAL_PRIVATE_PLATFORM_APIS := true
+LOCAL_CERTIFICATE := platform
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_MODULE_TAGS := optional
+LOCAL_USE_AAPT2 := true
+
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../SuperiorSettings/src)
+
+LOCAL_STATIC_ANDROID_LIBRARIES := \
+    $(ANDROID_SUPPORT_DESIGN_TARGETS) \
+    android-slices-builders \
+    android-slices-core \
+    android-slices-view \
+    android-support-compat \
+    android-support-v4 \
+    android-support-v13 \
+    android-support-v7-appcompat \
+    android-support-v7-cardview \
+    android-support-v7-preference \
+    android-support-v7-recyclerview \
+    android-support-v14-preference \
+
+LOCAL_JAVA_LIBRARIES := \
+    bouncycastle \
+    telephony-common \
+    ims-common \
+    telephony-ext
+
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-arch-lifecycle-runtime \
+    android-arch-lifecycle-extensions \
+    guava \
+    jsr305 \
+    settings-logtags \
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    frameworks/support/v7/preference/res \
+    frameworks/support/v14/preference/res \
+    frameworks/support/v7/appcompat/res \
+    frameworks/support/v7/recyclerview/res \
+    packages/apps/SuperiorSettings/res
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages android.support.v7.preference \
+    --extra-packages android.support.v14.preference \
+    --extra-packages android.support.v17.preference \
+    --extra-packages android.support.v7.appcompat \
+    --extra-packages android.support.v7.recyclerview \
+    --extra-packages com.superior.settings
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+ifneq ($(INCREMENTAL_BUILDS),)
+    LOCAL_PROGUARD_ENABLED := disabled
+    LOCAL_JACK_ENABLED := incremental
+    LOCAL_JACK_FLAGS := --multi-dex native
+endif
+
+include frameworks/opt/setupwizard/library/common-gingerbread.mk
+include frameworks/base/packages/SettingsLib/common.mk
+
+include $(BUILD_PACKAGE)
+
+# Use the following include to make our test apk.
+ifeq (,$(ONE_SHOT_MAKEFILE))
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
